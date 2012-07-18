a074fe7c0cf7389e2d0f85d9cb7e8d31140f577e
Alfasado Inc <webmaster@alfasado.jp>
2012-07-18 17:28:44 +0900

    first commit

---
+++ plugins/SetCookie/php/function.mtclearcookie.php
@@ -0,0 +1,27 @@
+<?php
+function smarty_function_mtclearcookie ( $args, &$ctx ) {
+    $reload = $args[ 'reload' ];
+    $name   = $args[ 'name' ];
+    $cookie_val = $_COOKIE[ $name ];
+    $value  = '';
+    $path   = $args[ 'path' ];
+    if (! $path ) {
+        $path = '/';
+    }
+    $domain = $args[ 'domain' ];
+    $expires = time() - 3600;
+    $secure  = $args[ 'secure' ];
+    if ( setcookie( $name, $value, $expire, $path, $domain, $secure ) ) {
+        if ( $cookie_val != $value ) {
+            if ( $reload ) {
+                $app = $ctx->stash( 'bootstrapper' );
+                $return_url = $app->base() . $app->request;
+                if ( $query_string = $app->query_string() ) {
+                    $return_url .= '?' . $query_string;
+                }
+                header( 'Location: ' . $return_url );
+            }
+        }
+    }
+}
+?>
---
+++ plugins/SetCookie/php/function.mtsetcookie.php
@@ -0,0 +1,32 @@
+<?php
+function smarty_function_mtsetcookie ( $args, &$ctx ) {
+    $reload = $args[ 'reload' ];
+    $name   = $args[ 'name' ];
+    $cookie_val = $_COOKIE[ $name ];
+    $value  = $args[ 'value' ];
+    $path   = $args[ 'path' ];
+    if (! $path ) {
+        $path = '/';
+    }
+    $domain = $args[ 'domain' ];
+    $expires = $args[ 'expires' ];
+    if ( $expires ) {
+        $expires += time();
+    } else {
+        $expires = 0;
+    }
+    $secure = $args[ 'secure' ];
+    if ( setcookie( $name, $value, $expire, $path, $domain, $secure ) ) {
+        if ( $cookie_val != $value ) {
+            if ( $reload ) {
+                $app = $ctx->stash( 'bootstrapper' );
+                $return_url = $app->base() . $app->request;
+                if ( $query_string = $app->query_string() ) {
+                    $return_url .= '?' . $query_string;
+                }
+                header( 'Location: ' . $return_url );
+            }
+        }
+    }
+}
+?>
